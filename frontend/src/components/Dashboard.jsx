9935fa7d0a591e8f74c8c88ee231337d907b11e8
Musa Alfian Maulana <musaganteng071@gmail.com>
2025-01-04 15:23:44 +0700

    `feat(dark-mode)` create darkmode handling

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import userServices from '../services/user.service';
-import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import { Avatar, DarkThemeToggle, Dropdown, Navbar } from 'flowbite-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { authUser, logout } from '../redux/slices/userSlice';
 
@@ -34,38 +34,43 @@ const Dashboard = () => {
    return (
       <>
          {/* Navbar */}
-         <Navbar fluid rounded className='w-1/2 mx-auto !px-0'>
-            <Navbar.Brand href='https://flowbite-react.com'>
-               <span className='self-center whitespace-nowrap font-semibold dark:text-white'>Simple Dashboard</span>
-            </Navbar.Brand>
-            <div className='flex md:order-2'>
-               <Dropdown
-                  arrowIcon={false}
-                  inline
-                  label={
-                     <Avatar
-                        alt='User settings'
-                        img='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
-                        rounded
-                     />
-                  }
-               >
-                  <Dropdown.Header>
-                     <span className='block text-sm'>{authenticatedUser.name}</span>
-                     <span className='block truncate text-sm font-medium'>{authenticatedUser.email}</span>
-                  </Dropdown.Header>
-                  <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
-               </Dropdown>
-               <Navbar.Toggle />
+         <Navbar fluid className=' dark:bg-slate-600'>
+            <div className='w-1/2 mx-auto flex justify-between gap-3'>
+               <Navbar.Brand href='https://flowbite-react.com'>
+                  <span className='self-center whitespace-nowrap font-semibold dark:text-white'>Simple Dashboard</span>
+               </Navbar.Brand>
+               <div className='flex md:order-2 gap-3'>
+                  <Dropdown
+                     arrowIcon={false}
+                     inline
+                     label={
+                        <Avatar
+                           alt='User settings'
+                           img='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
+                           rounded
+                        />
+                     }
+                  >
+                     <Dropdown.Header>
+                        <span className='block text-sm'>{authenticatedUser.name}</span>
+                        <span className='block truncate text-sm font-medium'>{authenticatedUser.email}</span>
+                     </Dropdown.Header>
+                     <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
+                  </Dropdown>
+                  <Navbar.Toggle />
+                  <DarkThemeToggle />
+               </div>
             </div>
          </Navbar>
 
          {/* List users */}
-         <div className='grid w-full h-screen place-items-center bg-sky-100'>
+         <div className='grid w-full h-screen place-items-center bg-sky-100 dark:bg-slate-800'>
             <div className='w-1/2'>
-               <h1 className='text-gray-800 font-medium text-xl mb-4'>Welcome Back, {authenticatedUser.name}!</h1>
-               <div className='py-4 border border-gray-200 bg-white rounded-md'>
-                  <h3 className='font-bold border-b text-gray-800 pb-4 px-4'>List User</h3>
+               <h1 className='text-gray-800 font-medium text-xl mb-4 dark:text-white'>
+                  Welcome Back, {authenticatedUser.name}!
+               </h1>
+               <div className='py-4 border border-gray-200 bg-white dark:text-slate-100 dark:bg-slate-700 rounded-md'>
+                  <h3 className='font-bold border-b text-gray-800 pb-4 px-4 dark:text-white'>List User</h3>
                   {users.length > 0 ? (
                      users.map((user, idx) => (
                         <ul className='px-4 pt-4' key={idx}>
